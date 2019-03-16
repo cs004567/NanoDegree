216dfe5ddde9c1e37323d93660e67a09d289e4de
CSmith <csmith004567@gmail.com>
2019-03-16 14:43:11 +0000

    20190316 Edited for PEP8 compliance.

--- catalog/database_setup.py
+++ catalog/database_setup.py
@@ -2,8 +2,9 @@ from sqlalchemy import Column, ForeignKey, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 from sqlalchemy import create_engine
- 
+
 Base = declarative_base()
+
 
 class User(Base):
     __tablename__ = 'user'
@@ -13,6 +14,7 @@ class User(Base):
     email = Column(String(250), nullable=False)
     picture = Column(String(250))
 
+
 class CatalogCategory(Base):
     __tablename__ = 'category'
 
@@ -21,32 +23,33 @@ class CatalogCategory(Base):
 
     @property
     def serialize(self):
-       """Return data in serializeable format"""
-       return {
-           'id'          : self.id,
-           'name'        : self.name,
-       }
+        """Return data in serializeable format"""
+        return {
+            'id': self.id,
+            'name': self.name,
+        }
 
- 
+
 class CatalogItem(Base):
     __tablename__ = 'item'
 
-    id = Column(Integer, primary_key = True)
-    name = Column(String(250), nullable = False)
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
     description = Column(String(250))
-    category_id = Column(Integer,ForeignKey('category.id'))
+    category_id = Column(Integer, ForeignKey('category.id'))
     category = relationship(CatalogCategory)
     user_id = Column(Integer, ForeignKey('user.id'))
     user = relationship(User)
 
     @property
     def serialize(self):
-       """Return  data in serializeable format"""
-       return {
-           'id'          : self.id,
-           'name'        : self.name,
-           'description' : self.description,
-       }
+        """Return  data in serializeable format"""
+        return {
+            'id': self.id,
+            'name': self.name,
+            'description': self.description,
+        }
+
 
 engine = create_engine('sqlite:///catalog.db')
 
